f844e67f6344ce2171f1e12b9ffff04e1482b25c
Grace Njenga <76649048+Grace-Njenga@users.noreply.github.com>
2024-04-17 14:08:16 +0300

    small changes

---
+++ script.js
@@ -0,0 +1,11 @@
+const result = document.getElementById('display');
+const long = document.getElementById('long');
+const short = document.getElementById('short');
+const uppercase = document.getElementById('upper');
+const lowercase = document.getElementById('lower');
+const numbers = document.getElementById('numbers');
+const symbols = document.getElementById('special');
+const copy = document.getElementById('copy');
+const generate = document.getElementById('generate');
+const memorable = document.getElementById('memorable');
+
